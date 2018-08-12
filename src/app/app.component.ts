4e7d6717d1be2398884c13047626c3c71b6aa439
sidharthdec <sidharthdec@gmail.com>
2018-08-12 20:40:57 +0530

    practice angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng4-complete-guide';
+  loadedFeature = 'rcp';
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
